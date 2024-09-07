d22846b5e47af6706124da733029f8be43cabbbd
ChristianMDG <you@example.com>
2024-09-07 20:43:47 +0300

    Add function Loading

--- Gestion De Library X-Tends 1.0/src/main/java/com/X_Tends/Teams/library_management/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.X_Tends.Teams.library_management;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
-
---
+++ chargement.js
@@ -0,0 +1,7 @@
+function showLoading() {
+    document.getElementById('loading').style.display = 'block';
+}
+
+function hideLoading() {
+    document.getElementById('loading').style.display = 'none';
+}
